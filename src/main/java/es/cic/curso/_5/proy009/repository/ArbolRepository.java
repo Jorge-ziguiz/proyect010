0de7bb5c9dbcd47ac7fefea1421c414bbd48926f
curso15 <curso15@noquiero.com>
2025-07-31 19:25:09 +0200

    hola

--- src/main/java/es/cic/curso/_5/proy009/repository/ArbolRepository.java
+++
@@ -1,9 +0,0 @@
-package es.cic.curso._5.proy009.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import es.cic.curso._5.proy009.model.Arbol;
-
-public interface ArbolRepository extends JpaRepository<Arbol, Long> {
-
-}
--- src/main/java/es/cic/curso/_5/proy009/repository/RamaRepository.java
+++
@@ -1,9 +0,0 @@
-package es.cic.curso._5.proy009.repository;
-
-import es.cic.curso._5.proy009.model.Rama;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface RamaRepository extends JpaRepository<Rama, Long> {
-
-}
